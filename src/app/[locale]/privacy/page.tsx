8156c4b527c02527076c508b8cdf9ae3948398f2
nivance <limingjian110@163.com>
2025-03-27 21:12:05 +0800

    sitename to param

--- src/app/[locale]/privacy/page.tsx
+++ src/app/[locale]/privacy/page.tsx
@@ -1,15 +1,17 @@
 import { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
 import { config } from "@/lib/config";
+import { removeProtocolFromUrl } from "@/lib/utils";
 
 export const runtime = "edge";
 
 export async function generateMetadata(): Promise<Metadata> {
   const canonicalUrl = `${config.baseUrl}/privacy`;
   const t = await getTranslations('metadata');
+  const siteName = removeProtocolFromUrl(config.baseUrl);
   return {
-    title: 'Privacy Policy | blockbreaker.im',
-    description: 'blockbreaker.im privacy policy',
+    title: 'Privacy Policy | ' + siteName,
+    description: siteName + ' privacy policy',
     keywords: t("keywords"),
     icons: [
       {
@@ -24,6 +26,7 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default function Page() {
+  const siteName = removeProtocolFromUrl(config.baseUrl);
   return (
     <>
       <section>
@@ -31,7 +34,7 @@ export default function Page() {
           <div className="flex h-auto min-w-[80%] flex-col items-center justify-end py-6 md:h-64">
             <div className="flex flex-col items-center gap-y-4 py-5">
               <h1 className="text-3xl font-bold md:text-5xl">Privacy Policy</h1>
-              <p className="text-sm text-[#808080] sm:text-base">Last Updated as of  January 25, 2025</p>
+              <p className="text-sm text-[#808080] sm:text-base">Last Updated as of March 29, 2025</p>
             </div>
           </div>
           <div className="mx-auto w-full max-w-5xl py-6">
@@ -43,19 +46,19 @@ export default function Page() {
                   <div className="flex min-w-full flex-col items-start gap-y-6">
                     <div className="flex flex-col items-start gap-y-3">
                       <p className="text-sm font-bold">Overview</p>
-                      <p className="text-sm">Welcome to <a href="https://blockbreaker.im" className="font-bold underline text-blue-600">https://blockbreaker.im</a>. This privacy policy outlines the types of information we collect, how we use it, and the steps we take to ensure your personal information is handled appropriately.</p>
+                      <p className="text-sm">Welcome to <a href={config.baseUrl} className="font-bold underline text-blue-600">{config.baseUrl}</a>. This privacy policy outlines the types of information we collect, how we use it, and the steps we take to ensure your personal information is handled appropriately.</p>
                     </div>
                     <div className="flex flex-col items-start gap-y-3">
                       <p className="text-sm font-bold">Cookies</p>
-                      <p className="text-sm">To enrich and perfect your online experience, blockbreaker.im uses "Cookies", similar technologies and services provided by others to display personalized 
+                      <p className="text-sm">To enrich and perfect your online experience, {siteName} uses "Cookies", similar technologies and services provided by others to display personalized 
                         content, appropriate advertising and store your preferences on your computer.
                       </p>
                       <p className="text-sm">A cookie is a string of information that a website stores on a visitor's computer, and that the visitor's browser provides to the website 
-                        each time the visitor returns. blockbreaker.im uses cookies to help blockbreaker.im identify and track visitors, their usage of blockbreaker.im, and their website 
-                        access preferences. blockbreaker.im visitors who do not wish to have cookies placed on their computers should set their browsers to refuse cookies before using 
-                        blockbreaker.im's websites, with the drawback that certain features of blockbreaker.im's websites may not function properly without the aid of cookies.
+                        each time the visitor returns. {siteName} uses cookies to help {siteName} identify and track visitors, their usage of {siteName}, and their website 
+                        access preferences. {siteName} visitors who do not wish to have cookies placed on their computers should set their browsers to refuse cookies before using 
+                        {siteName}'s websites, with the drawback that certain features of {siteName}'s websites may not function properly without the aid of cookies.
                       </p>
-                      <p className="text-sm">By continuing to navigate our website without changing your cookie settings, you hereby acknowledge and agree to blockbreaker.im's use of cookies.
+                      <p className="text-sm">By continuing to navigate our website without changing your cookie settings, you hereby acknowledge and agree to {siteName}'s use of cookies.
                       </p>
                     </div>
                     <div className="flex flex-col items-start gap-y-3">
@@ -65,24 +68,24 @@ export default function Page() {
                     </div>
                     <div className="flex flex-col items-start gap-y-3">
                       <p className="text-sm font-bold">Aggregated Statistics</p>
-                      <p className="text-sm">blockbreaker.im may collect statistics about the behavior of visitors to its website. blockbreaker.im don't display this information publicly or provide it to others, 
-                        and blockbreaker.im will not disclose your personally-identifying information.
+                      <p className="text-sm">{siteName} may collect statistics about the behavior of visitors to its website. {siteName} don't display this information publicly or provide it to others, 
+                        and {siteName} will not disclose your personally-identifying information.
                       </p>
                     </div>
                     <div className="flex flex-col items-start gap-y-3">
                       <p className="text-sm font-bold">Data Sharing</p>
-                      <p className="text-sm">blockbreaker.im respects your privacy. We do not share your personal data with any third parties, except as necessary to process your orders or comply with legal requirements.</p>
+                      <p className="text-sm">{siteName} respects your privacy. We do not share your personal data with any third parties, except as necessary to process your orders or comply with legal requirements.</p>
                     </div>
                     <div className="flex flex-col items-start gap-y-3">
                       <p className="text-sm font-bold">Privacy Policy Changes</p>
-                      <p className="text-sm">Although most changes are likely to be minor, blockbreaker.im may change its Privacy Policy from time to time, and in blockbreaker.im's sole discretion.
-                         blockbreaker.im encourages visitors to frequently check this page for any changes to its Privacy Policy. Your continued use of this site after any change in this Privacy Policy 
+                      <p className="text-sm">Although most changes are likely to be minor, {siteName} may change its Privacy Policy from time to time, and in {siteName}'s sole discretion.
+                         {siteName} encourages visitors to frequently check this page for any changes to its Privacy Policy. Your continued use of this site after any change in this Privacy Policy 
                          will constitute your acceptance of such change.
                       </p>
                     </div>
                     <div className="flex flex-col items-start gap-y-3">
                       <p className="text-sm font-bold">Contact Us</p>
-                      <p className="text-sm">If you have any questions, concerns, or feedback about our Privacy Policy, please contact us support@blockbreaker.im. We are committed to addressing your inquiries and will strive to respond as promptly as possible.</p>
+                      <p className="text-sm">If you have any questions, concerns, or feedback about our Privacy Policy, please contact us support@{siteName}. We are committed to addressing your inquiries and will strive to respond as promptly as possible.</p>
                     </div>
                   </div>
                 </div>
--- src/app/[locale]/termsofservice/page.tsx
+++ src/app/[locale]/termsofservice/page.tsx
@@ -1,15 +1,17 @@
 import { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
 import { config } from "@/lib/config";
+import { removeProtocolFromUrl } from "@/lib/utils";
 
 export const runtime = "edge";
 
 export async function generateMetadata(): Promise<Metadata> {
   const canonicalUrl = `${config.baseUrl}/termsofservice`;
   const t = await getTranslations('metadata');
+  const siteName = removeProtocolFromUrl(config.baseUrl);
   return {
-    title: 'TERMS OF SERVICE | blockbreaker.im',
-    description: 'blockbreaker.im terms of service',
+    title: 'TERMS OF SERVICE | ' + siteName,
+    description: siteName + ' terms of service',
     keywords: t("keywords"),
     icons: [
       {
@@ -24,13 +26,14 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default function Page() {
+  const siteName = removeProtocolFromUrl(config.baseUrl);
   return (
     <section>
       <div className="flex flex-col items-center justify-center px-5 md:px-10">
         <div className="flex h-auto min-w-[80%] flex-col items-center justify-end py-6 md:h-64">
           <div className="flex flex-col items-center gap-y-4 py-5">
             <h1 className="text-3xl font-bold md:text-5xl">TERMS OF SERVICE</h1>
-            <p className="text-sm text-[#808080] sm:text-base">Last Updated as of January 20, 2025</p>
+            <p className="text-sm text-[#808080] sm:text-base">Last Updated as of March 29, 2025</p>
           </div>
         </div>
         <div className="mx-auto w-full max-w-5xl py-6">
@@ -43,52 +46,52 @@ export default function Page() {
                 <div className="flex min-w-full flex-col items-start gap-y-6">
                   <div className="flex flex-col items-start gap-y-3">
                     <p className="text-sm font-bold">Introduction</p>
-                    <p className="text-sm">Welcome to blockbreaker.im. By accessing our website (<a href="https://blockbreaker.im" className="font-bold underline text-blue-600">https://blockbreaker.im</a>), 
+                    <p className="text-sm">Welcome to {siteName}. By accessing our website (<a href={config.baseUrl} className="font-bold underline text-blue-600">{config.baseUrl}</a>), 
                       you agree to be bound by these Terms of Service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. 
                       If you do not agree with any of these terms, you are prohibited from using or accessing this site.
                     </p>
                   </div>
                   <div className="flex flex-col items-start gap-y-3">
                     <p className="text-sm font-bold">Website Visitors</p>
-                    <p className="text-sm">Like most website operators, <a href="https://blockbreaker.im" className="font-bold underline text-blue-600">https://blockbreaker.im</a> collects
+                    <p className="text-sm">Like most website operators, <a href={config.baseUrl} className="font-bold underline text-blue-600">{config.baseUrl}</a> collects
                       non-personally-identifying information of the sort that web browsers and servers typically make available, such as the browser type, 
-                      language preference, referring site, and the date and time of each visitor request. blockbreaker.im's purpose in collecting non-personally identifying information 
-                      is to better understand how blockbreaker.im's visitors use its website. From time to time, blockbreaker.im may release non-personally-identifying information 
+                      language preference, referring site, and the date and time of each visitor request. {siteName}'s purpose in collecting non-personally identifying information 
+                      is to better understand how {siteName}'s visitors use its website. From time to time, {siteName} may release non-personally-identifying information 
                       in the aggregate, e.g., by publishing a report on trends in the usage of its website.
                     </p>
                     <p className="text-sm">
-                      blockbreaker.im also collects potentially personally-identifying information like Internet Protocol (IP) addresses for logged in users and 
-                      for users leaving comments on https://blockbreaker.im/ blog posts. blockbreaker.im only discloses logged in user and commenter IP addresses 
+                      {siteName} also collects potentially personally-identifying information like Internet Protocol (IP) addresses for logged in users and 
+                      for users leaving comments on {config.baseUrl} blog posts. {siteName} only discloses logged in user and commenter IP addresses 
                       under the same circumstances that it uses and discloses personally-identifying information as described below.
                     </p>
                   </div>
                   <div className="flex flex-col items-start gap-y-3">
                     <p className="text-sm font-bold">Aggregated Statistics</p>
-                    <p className="text-sm">blockbreaker.im may collect statistics about the behavior of visitors to its website. blockbreaker.im don't display this information publicly or provide it to others, 
-                      and blockbreaker.im will not disclose your personally-identifying information.
+                    <p className="text-sm">{siteName} may collect statistics about the behavior of visitors to its website. {siteName} don't display this information publicly or provide it to others, 
+                      and {siteName} will not disclose your personally-identifying information.
                     </p>
                   </div>
                   <div className="flex flex-col items-start gap-y-3">
                     <p className="text-sm font-bold">Cookies</p>
-                    <p className="text-sm">To enrich and perfect your online experience, blockbreaker.im uses "Cookies", similar technologies and services provided by others to display personalized 
+                    <p className="text-sm">To enrich and perfect your online experience, {siteName} uses "Cookies", similar technologies and services provided by others to display personalized 
                       content, appropriate advertising and store your preferences on your computer.
                     </p>
                     <p className="text-sm">A cookie is a string of information that a website stores on a visitor's computer, and that the visitor's browser provides to the website 
-                      each time the visitor returns. blockbreaker.im uses cookies to help blockbreaker.im identify and track visitors, their usage of blockbreaker.im, and their website 
-                      access preferences. blockbreaker.im visitors who do not wish to have cookies placed on their computers should set their browsers to refuse cookies before using 
-                      blockbreaker.im's websites, with the drawback that certain features of blockbreaker.im's websites may not function properly without the aid of cookies.
+                      each time the visitor returns. {siteName} uses cookies to help {siteName} identify and track visitors, their usage of {siteName}, and their website 
+                      access preferences. {siteName} visitors who do not wish to have cookies placed on their computers should set their browsers to refuse cookies before using 
+                      {siteName}'s websites, with the drawback that certain features of {siteName}'s websites may not function properly without the aid of cookies.
                     </p>
-                    <p className="text-sm">By continuing to navigate our website without changing your cookie settings, you hereby acknowledge and agree to blockbreaker.im's use of cookies.
+                    <p className="text-sm">By continuing to navigate our website without changing your cookie settings, you hereby acknowledge and agree to {siteName}'s use of cookies.
                     </p>
                   </div>
                   <div className="flex flex-col items-start gap-y-3">
                     <p className="text-sm font-bold">Limitations</p>
-                    <p className="text-sm">In no event shall blockbreaker.im or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) 
-                      arising out of the use or inability to use the materials on blockbreaker.im's website.</p>
+                    <p className="text-sm">In no event shall {siteName} or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) 
+                      arising out of the use or inability to use the materials on {siteName}'s website.</p>
                   </div>
                   <div className="flex flex-col items-start gap-y-3">
                     <p className="text-sm font-bold">Disclaimer</p>
-                    <p className="text-sm">The materials on blockbreaker.im's website are provided on an 'as is' basis. blockbreaker.im makes no warranties, expressed or implied, 
+                    <p className="text-sm">The materials on {siteName}'s website are provided on an 'as is' basis. {siteName} makes no warranties, expressed or implied, 
                       and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, 
                       fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.
                     </p>
@@ -104,10 +107,6 @@ export default function Page() {
                   <div className="flex flex-col items-start gap-y-3">
                     <p className="text-sm font-bold">Modifications</p>
                     <p className="text-sm">We may revise these Terms of Service for its website at any time without notice. By using this website, you are agreeing to be bound by the then current version of these Terms of Service.</p>
-                  </div>
-                  <div className="flex flex-col items-start gap-y-3">
-                    <p className="text-sm font-bold">Contact Us</p>
-                    <p className="text-sm">If you have any questions, concerns, or feedback about our Privacy Policy, please contact us support@blockbreaker.im. We are committed to addressing your inquiries and will strive to respond as promptly as possible.</p>
                   </div>
                 </div>
               </div>
