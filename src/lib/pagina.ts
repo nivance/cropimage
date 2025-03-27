c3edb55eec3803d7d3d25f0df2fb3d861945e3df
nivance <limingjian110@163.com>
2025-03-27 20:44:58 +0800

    remove

--- src/lib/pagina.ts
+++
@@ -1,7 +0,0 @@
-export interface Pagina {
-    page: number;
-    limit: number | "all";
-    totalPages: number;
-    nextPage: number | null;
-    prevPage: number | null;
-}
--- src/models/types/game.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Game {
-    name: string;
-    title: string;
-    image: string;
-    src: string;
-    type: string[];
-    related: string[];
-  }
