57ceb42a922207d4cf0fa127eefa4130146af3da
nivance <limingjian110@163.com>
2025-03-28 23:47:39 +0800

    add generateMetadata

--- src/app/[locale]/convert-webp-to-jpg/page.tsx
+++ src/app/[locale]/convert-webp-to-jpg/page.tsx
@@ -1,5 +1,28 @@
 import ImageConverterPage from '@/components/converter';
 import WhyChooseUsConverter from '@/components/common/WhyChooseUsConverter';
+import { Metadata } from 'next';
+import { config } from '@/lib/config';
+
+export const runtime = "edge";
+
+export async function generateMetadata(context: { params: { locale: string } }): Promise<Metadata> {
+  const { params } = await context;
+  const { locale } = await params;
+  return {
+    title: "Convert WebP to JPG",
+    description: "Quickly convert your WEBP images to JPG format. Free online conversion with no software installation required. Maintain quality and convert in seconds.",
+    keywords: "Convert WebP to JPG, Image Converter, Convert WebP to JPG Online, WebP to JPG High Quality, Best WebP to JPG Converter",
+    icons: [
+      {
+        rel: 'icon',
+        url: '/favicon.ico',
+      },
+    ],
+    alternates: {
+      canonical: `${config.baseUrl}/${locale !== "en" ? locale + "/" : ""}convert-webp-to-jpg`,
+    }
+  };
+}
 
 export default function Page() {
 
