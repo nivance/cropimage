b148428bc79380e8a6b2e9da7555ab638123f50f
nivance <limingjian110@163.com>
2025-03-29 07:43:17 +0800

    add convert-jpg-to-webp

---
+++ src/app/[locale]/convert-jpg-to-webp/page.tsx
@@ -0,0 +1,35 @@
+import ImageConverterPage from '@/components/converter';
+import WhyChooseUsConverter from '@/components/common/WhyChooseUsConverter';
+import { Metadata } from 'next';
+import { config } from '@/lib/config';
+
+export const runtime = "edge";
+
+export async function generateMetadata(context: { params: { locale: string } }): Promise<Metadata> {
+  const { params } = await context;
+  const { locale } = await params;
+  return {
+    title: "Convert JPG to WEBP",
+    description: "Quickly convert your JPG images to WEBP format. Free online conversion with no software installation required. Maintain quality and convert in seconds.",
+    keywords: "Convert JPG to WEBP, Image Converter, Convert JPG to WEBP Online, JPG to WEBP High Quality, Best JPG to WEBP Converter",
+    icons: [
+      {
+        rel: 'icon',
+        url: '/favicon.ico',
+      },
+    ],
+    alternates: {
+      canonical: `${config.baseUrl}/${locale !== "en" ? locale + "/" : ""}convert-JPG-to-WEBP`,
+    }
+  };
+}
+
+export default function Page() {
+
+  return (
+    <div className="py-6">
+      <ImageConverterPage source="JPG" target="WEBP" />
+      <WhyChooseUsConverter />
+    </div>
+  );
+}
