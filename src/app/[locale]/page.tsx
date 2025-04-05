433d755b9fc84a62a3bcbf771122fea496a3ea70
nivance <limingjian110@163.com>
2025-04-05 17:57:01 +0800

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,9 +8,9 @@ import { config } from '@/lib/config';
 
 export const runtime = "edge";
 
-export async function generateMetadata(context: { params: { locale: string } }): Promise<Metadata> {
-  const { params } = await context;
-  const { locale } = await params;
+export async function generateMetadata({ params }: { params: { locale: string } }): Promise<Metadata> {
+  const parameters = await params;
+  const locale = await parameters.locale;
   const t = await getTranslations('metadata');
   return {
     title: t("title"),
@@ -30,11 +30,15 @@ export async function generateMetadata(context: { params: { locale: string } }):
 
 export default function Home() {
   return (
-    <div className="min-h-screen py-10">
-      <CropSection />
-      <HowToUse area='howToUse'/>
-      <WhyChooseUs />
-      <FAQs faqInfo='faqInfo' faqs='faqs' faqKeys={['q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10', 'q11', 'q12', 'q13', 'q14', 'q15', 'q16', 'q17', 'q18', 'q19', 'q20']} />
-    </div>
+    <>
+      <main>
+        <div className="min-h-screen py-10">
+          <CropSection />
+          <HowToUse area='howToUse' />
+          <WhyChooseUs />
+          <FAQs faqInfo='faqInfo' faqs='faqs' faqKeys={['q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10', 'q11', 'q12', 'q13', 'q14', 'q15', 'q16', 'q17', 'q18', 'q19', 'q20']} />
+        </div>
+      </main>
+    </>
   );
 }
